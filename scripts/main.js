944de4aea1f5269ebe8ae5e2d1cc6520382af59f
ldigalle <34316414+ladigalle@users.noreply.github.com>
2024-12-18 11:32:15 +0800

    Update app install process

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,53 @@
+// Remove install button and banner
+const checkInstall = () => {
+    installButton.hidden = true;
+    installBanner.hidden = true;
+    preDebug.append("App installed");
+}
+
+// Register serviceWorker on page loading
 window.onload = () => {
     "use strict";
 
     if ("serviceWorker" in navigator && document.URL.split(":")[0] !== "file") {
         navigator.serviceWorker.register("./../sw.js");
     }
+
+    if (window.matchMedia('(display-mode: standalone)').matches) {  
+        checkInstall();
+    }  
 }
 
+// Catch PWA install prompt and manage it 
 window.addEventListener("beforeinstallprompt", e => {
-    pre.append(`> beforeinstallprompt fired with platforms: ${e.platforms.join(',')}\r\n`);
-    const showDefaultPrompt = new URLSearchParams(location.search).has(
-      "showDefaultPrompt"
-    );
+    preDebug.append("Waiting for beforeinstallprompt...\r\n");
+    const showDefaultPrompt = new URLSearchParams(location.search).has("showDefaultPrompt");
+    preDebug.append(`> beforeinstallprompt fired with platforms: ${e.platforms.join(',')}\r\n`);
+    
+    
     if (showDefaultPrompt) {
-      pre.append(
+      preDebug.append(
         "Default PWA mini info-bar on mobile should show up. Clear site settings if not.\r\n"
       );
       return;
     }
-    promptButton.hidden = false;
+
+    installButton.hidden = false;
+    installBanner.hidden = false;
+    
     e.preventDefault();
+    
     e.userChoice.then(result =>
-      pre.append(`userChoice resolved with: ${JSON.stringify(result)}\r\n`)
+        preDebug.append(`userChoice resolved with: ${JSON.stringify(result)}\r\n`)
     );
-    promptButton.onclick = () => {
-      pre.append(
-        "Waiting for user choice...\r\n"
-      );
+    
+    installButton.onclick = () => {
+        preDebug.append("Waiting for user choice...\r\n");
       e.prompt();
     };
-  });
+});
+
+// Check if app is installed 
+window.addEventListener("appinstalled", e => {
+    checkInstall();
+})
